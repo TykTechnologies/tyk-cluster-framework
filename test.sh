1ffbb344c8e1dbb6bd103d1ed6c4fe02af7d8e37
Martin Buhr <martin@tyk.io>
2017-03-24 12:20:53 +1300

    Changed order

--- test.sh
+++ test.sh
@@ -5,14 +5,14 @@ echo "Testing verifier/"
 cd verifier
 go test -v
 cd ..
-echo "Testing client/"
-cd client
-go test -v
-cd ..
 echo "Testing server/"
 cd server
 go test -v
 cd ..
+echo "Testing client/"
+cd client
+go test -v
+cd ..
 echo "Testing distributed_store/"
 cd distributed_store
-go test -v 
+go test -v
